8e6d34ae394a460a05d08daf41437d79eb076ae7
saidnahum <saidnahum@gmail.com>
2021-11-24 14:36:51 -0600

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a342828b68ae9ee2c072d45ed158410d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ca8bf098-c84f-477c-9830-69dcf885336d'
+};
